6de82ddda6af0b487a9f65178728250a9dc9fc65
HSZGB <1812748986@qq.com>
2025-04-17 13:31:43 +0800

    init lab3

--- pkg/kernel/src/main.rs
+++ pkg/kernel/src/main.rs
@@ -1,28 +1,3 @@
-// #![no_std]
-// #![no_main]
-
-// #[macro_use]
-// extern crate log;
-
-// use core::arch::asm;
-// use ysos_kernel as ysos;
-
-// boot::entry_point!(kernel_main);
-
-// pub fn kernel_main(boot_info: &'static boot::BootInfo) -> ! {
-//     ysos::init(boot_info);
-
-//     loop {
-//         info!("Hello World from YatSenOS v2!");
-
-//         for _ in 0..0x10000000 {
-//             unsafe {
-//                 asm!("nop");
-//             }
-//         }
-//     }
-// }
-
 #![no_std]
 #![no_main]
 
@@ -36,16 +11,30 @@ boot::entry_point!(kernel_main);
 pub fn kernel_main(boot_info: &'static boot::BootInfo) -> ! {
     ysos::init(boot_info);
 
-    loop {
-        print!("> ");
-        let input = input::get_line();
+    // FIXME: update lib.rs to pass following tests
 
-        match input.trim() {
+    // 1. run some (about 5) "test", show these threads are running concurrently
+
+    // 2. run "stack", create a huge stack, handle page fault properly
+
+    let mut test_num = 0;
+
+    loop {
+        print!("[>] ");
+        let line = input::get_line();
+        match line.trim() {
             "exit" => break,
-            _ => {
-                println!("You said: {}", input);
-                println!("The counter value is {}", interrupt::clock::read_counter());
+            "ps" => {
+                ysos::proc::print_process_list();
             }
+            "stack" => {
+                ysos::new_stack_test_thread();
+            }
+            "test" => {
+                ysos::new_test_thread(format!("{}", test_num).as_str());
+                test_num += 1;
+            }
+            _ => println!("[=] {}", line),
         }
     }
 
